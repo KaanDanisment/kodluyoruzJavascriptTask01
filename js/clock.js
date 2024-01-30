735e9cb3fdf401bc49aff20ae5275a01a54d919f
Kaan Danisment <kaandanisment1qgmail.com>
2024-01-30 13:24:48 +0300

    task 1

---
+++ js/clock.js
@@ -0,0 +1,15 @@
+let myNameDom = document.querySelector("#myName")
+let myName = prompt("Adınızı girin")
+myNameDom.innerHTML = `${myName}`
+
+setInterval(function(){
+    let now = new Date()
+    let options = {hour: 'numeric', minute: 'numeric', second: 'numeric', weekday: 'long'};
+    let formattedTimeAndDay = now.toLocaleDateString('tr-TR', options);
+
+    let date = document.querySelector("#myClock")
+
+    date.innerHTML = formattedTimeAndDay
+},1000)
+
+
